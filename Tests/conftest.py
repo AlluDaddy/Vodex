ddb46dc96f8122854c4da0c7e65f640f6afc591c
AlluDaddy <60499478+AlluDaddy@users.noreply.github.com>
2023-04-05 15:48:29 +0530

    pass or fail screenshot

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -36,7 +36,8 @@ def driver_init(request):
 def _capture_screenshot(name):
     driver.get_screenshot_as_file(name)
 
-
+    
+ 
 @pytest.mark.hookwrapper
 def pytest_runtest_makereport(item):
     """
@@ -50,14 +51,38 @@ def pytest_runtest_makereport(item):
 
     if report.when == 'call' or report.when == "setup":
         xfail = hasattr(report, 'wasxfail')
-        if (report.skipped and xfail) or (report.failed and not xfail):
-            file_name = report.nodeid.replace("::", "_") + ".png"
-            _capture_screenshot(file_name)
-            if file_name:
-                html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
-                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
-                extra.append(pytest_html.extras.html(html))
+        # if (report.failed and xfail) or (report.failed and not xfail):
+        print(report.nodeid)
+        file_name = report.nodeid.replace("::", "_") + ".png"
+        _capture_screenshot(file_name)
+        if file_name:
+            html = '<div><img src="%s" alt="screenshot" style="width:204px;height:128px;" ' \
+                   'onclick="window.open(this.src)" align="right"/></div>' % file_name
+            extra.append(pytest_html.extras.html(html))
         report.extra = extra
+ 
+
+# @pytest.mark.hookwrapper
+# def pytest_runtest_makereport(item):
+#     """
+#     Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
+#     :param item:
+#     """
+#     pytest_html = item.config.pluginmanager.getplugin('html')
+#     outcome = yield
+#     report = outcome.get_result()
+#     extra = getattr(report, 'extra', [])
+
+#     if report.when == 'call' or report.when == "setup":
+#         xfail = hasattr(report, 'wasxfail')
+#         if (report.skipped and xfail) or (report.failed and not xfail):
+#             file_name = report.nodeid.replace("::", "_") + ".png"
+#             _capture_screenshot(file_name)
+#             if file_name:
+#                 html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
+#                        'onclick="window.open(this.src)" align="right"/></div>' % file_name
+#                 extra.append(pytest_html.extras.html(html))
+#         report.extra = extra
 
 
 class Test_BasicTest(BaseTest):
